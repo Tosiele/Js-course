2afc724a7ae6bf988fd0cca224631dc12d9b728c
Tosiele <tosiadulak@gmail.com>
2023-01-30 18:18:02 +0100

    [Refactor]: Setting up to do the course's homework

---
+++ podstawy/wisielec.js
@@ -0,0 +1,33 @@
+/*
+Gracz ma odgadnąć słowo
+Ma ograniczoną ilość prób
+Na początku zna długość słowa
+
+W każdej próbie może podać jedną literę:
+- Jeżeli znajduje się w słowie - każde jej wystąpienie jest ujawniane na odpowiednim miejscu
+- Jeżeli nie znajduje się - otrzymuje informację, że nie ma takiej litery w słowie
+
+Gra kończy się gdy słowo zosatnie odgadnięte lub skończą się próby
+
+UŻYĆ:
+zmienne
+tablice
+pętle
+funkcje
+biblioteki zewnętrzne (już zainstalowana)
+
+ZAŁOŻYĆ:
+stała lista słów do odgadnięcia
+
+DODATKOWO:
+rysuje się wisielec po nieudanej próbie
+_________
+|/      |
+|       O
+|       ^
+|      /|\
+|       ^
+|      / \
+*/
+
+
