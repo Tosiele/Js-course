b8c9283ba3eec9074db5b7eaaf56e595b5a60f72
Tosiele <tosiadulak@gmail.com>
2023-02-01 11:57:22 +0100

    Feat: Function taking json about currency from api Function taking course value from json Output of course

--- rozszerzenie/waluty.js
+++ rozszerzenie/waluty.js
@@ -22,3 +22,43 @@ http://api.nbp.pl/
 Dla chętnych:
 ładne formatowanie daty - biblioteka moment.js
 */
+
+/*
+funkcja pobierająca dane z api - USD
+funkcja wyświetlająca datę (w niej funkcja formatująca datę)
+to wszystko w set intervall które będzie wypisywać rezultaty tych funkcji
+
+potem funkcja zapisująca dane w pliku txt i zapisywała go
+*/
+
+import fetch from 'node-fetch';
+
+//const data = await fetch("http://api.nbp.pl/api/exchangerates/rates/c/usd/today/");
+//const json = await data.json();
+//const walutaJson = json.rates[0];
+
+let data; 
+data = await pobierzJson(data);
+console.log(data);
+
+let kurs;
+kurs = await pobierzKurs(data);
+console.log(kurs);
+//const ratesJson = data.rates[0];
+//const ratesArray = Object.values(ratesJson);
+//console.log(ratesArray[2]);
+
+
+
+async function pobierzJson(dane){
+    dane = await fetch("http://api.nbp.pl/api/exchangerates/rates/c/usd/today/");
+    const json = await dane.json();
+    return (json);
+}
+
+function pobierzKurs(json){
+    const jsonRates = json.rates[0];
+    const arrayRates = Object.values(jsonRates);
+    const currency = arrayRates[2];
+    return currency;
+}
